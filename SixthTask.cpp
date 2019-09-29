7b91d17a20e6c36e178c4e5fd96c1a3be9ce2e76
ectuser <m.ectuser@gmail.com>
2019-09-29 11:19:43 +0700

    First task solution. Nice code using cpp

---
+++ SixthTask.cpp
@@ -0,0 +1,133 @@
+#define _CRT_SECURE_NO_WARNINGS	
+#define _CRT_SECURE_NO_DEPRECATE
+#define _USE_MATH_DEFINES
+#include "pch.h"
+#include <iostream>
+#include <vector>
+#include <algorithm>
+#include <cmath>
+#include <string>
+#include <set>
+#include <vector>
+#include <map>
+#include <climits>
+#include <sstream>
+#include <ctime>
+#include <iomanip>
+#include <regex>
+#include <random>
+#include <queue>
+#include <stack>
+#include <cstdio>
+#include <cstdlib>
+#include <numeric>
+#include <cstring>
+#include <complex>
+#include <cassert>
+#include <iterator>
+#include <functional>
+#include <fstream>
+using namespace std;
+
+struct coordinates 
+{ 
+	int x1, y1, x2, y2, x3, y3;
+};
+
+int get_number()
+{
+	int n;
+	cin >> n;
+	return n;
+}
+
+coordinates get_triangle()
+{
+	coordinates tr;
+	cin >> tr.x1 >> tr.y1 >> tr.x2 >> tr.y2 >> tr.x3 >> tr.y3;
+	return tr;
+}
+
+vector<coordinates> create_coord_array(int n) {
+	vector<coordinates> coords(n);
+	return coords;
+}
+
+vector<coordinates> triangle_loop(int index, vector<coordinates> triangles)
+{
+	if (index >= triangles.size())
+	{
+		return triangles;
+	}
+	triangles[index] = get_triangle();
+	return triangle_loop(index + 1, triangles);
+	
+}
+
+coordinates triangle_multiplication(int mul_number, coordinates triangle)
+{
+	// one particular triangle
+	triangle.x1 *= mul_number;
+	triangle.y1 *= mul_number;
+	triangle.x2 *= mul_number;
+	triangle.y2 *= mul_number;
+	triangle.x3 *= mul_number;
+	triangle.y3 *= mul_number;
+	return triangle;
+}
+
+vector<coordinates> get_triangles_multiplication(int mul_number, vector<coordinates> triangles, int index) {
+	// adding all triangles together
+	if (index >= triangles.size()) {
+		return triangles;
+	}
+	triangles[index] = triangle_multiplication(mul_number, triangles[index]);
+	return get_triangles_multiplication(mul_number, triangles, index + 1);
+}
+
+coordinates count_new_vector(vector<coordinates> triangles, int index) {
+	coordinates new_vector;
+	new_vector.x1 = triangles[index].x2 - triangles[index].x1;
+	new_vector.y1 = triangles[index].y2 - triangles[index].y1;
+	// first math vector
+
+	new_vector.x2 = triangles[index].x3 - triangles[index].x2;
+	new_vector.y2 = triangles[index].y3 - triangles[index].y2;
+	// second math vector
+
+	new_vector.x3 = triangles[index].x1 - triangles[index].x3;
+	new_vector.y3 = triangles[index].y1 - triangles[index].y3;
+	// third math vector
+
+
+	return new_vector;
+}
+
+vector<coordinates> get_vectors(vector<coordinates> triangles, int index, vector<coordinates> vectors)
+{
+	if (index >= triangles.size()) {
+		return vectors;
+	}
+	vectors.push_back(count_new_vector(triangles, index)); // add math vectors (of one triangle) to vectors array
+	return get_vectors(triangles, index + 1, vectors);
+	
+}
+
+void show(vector<coordinates> result, int i)
+{
+	if (i < result.size())
+	{
+		cout << result[i].x1 << " " << result[i].y1 << " " << result[i].x2 << " " << result[i].y2 << " " << result[i].x3 << " " << result[i].y3 << endl;
+		show(result, i + 1);
+	}
+}
+
+vector<coordinates> create_empty_vector() {
+	vector<coordinates> vectors;
+	return vectors;
+}
+
+int main()
+{
+	show(get_vectors(get_triangles_multiplication(get_number(), triangle_loop(0, create_coord_array(get_number())), 0), 0, create_empty_vector()), 0);
+}
